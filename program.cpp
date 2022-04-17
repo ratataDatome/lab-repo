f1fa919fe376bb352a0cbba57949be810f33c792
Aleksandar Pandiloski <a.pandiloski@gmail.com>
2022-04-17 14:04:46 +0200

    test commit

---
+++ program.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+
+void printMyName () {
+	cout<<"Aleksandar Pandiloski";
+}
+
+void printMyIndex () {
+	cout<<"171051";
+}
+
+int main () {
+	printMyName();
+	printMyIndex();
+}
